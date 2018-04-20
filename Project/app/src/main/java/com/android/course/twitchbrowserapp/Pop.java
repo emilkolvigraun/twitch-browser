65b2e65a3c43aefceba254a30cd7653e019445b4
Emil Stubbe Kolvig-Raun <emilkolvigraun@gmail.com>
2018-04-20 19:55:30 +0200

    now streaming selected data from selected game source (top 1 streamer)

--- Project/app/src/main/java/com/android/course/twitchbrowserapp/Pop.java
+++ Project/app/src/main/java/com/android/course/twitchbrowserapp/Pop.java
@@ -18,7 +18,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -26,6 +29,9 @@ import java.util.Map;
  */
 
 public class Pop extends Activity {
+
+
+    private int width, height;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,91 +41,89 @@ public class Pop extends Activity {
         DisplayMetrics display = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(display);
 
-        int width = display.widthPixels;
-        int height = display.heightPixels;
+        this.width = display.widthPixels;
+        this.height = display.heightPixels;
 
-
-        TextView text = findViewById(R.id.textView_pop);
-        text.setText(User_Preferences.getSelection());
-
-        getWindow().setLayout((int)(width*0.7), (int)(height*0.5));
+        getWindow().setLayout((int)(this.width*0.7), (int)(this.height*0.5));
 
         requestWithSomeHttpHeaders();
+
     }
 
+    private void setPopUpContent(ArrayList<String> info){
+        TextView text = findViewById(R.id.textView_pop);
+        Log.d("DEBUGGING: ", info.toString());
+        text.setText(info.toString());
+        text.setAlpha(0f);
+        text.animate()
+                .setDuration(200)
+                .alpha(1f)
+                .setListener(null);
+
+
+    }
+
+    private boolean indexExists(final JSONArray list, final int index) {
+        return index >= 0 && index < list.length();
+    }
+
+
     public void requestWithSomeHttpHeaders() {
+
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
 
-        String url = "https://api.twitch.tv/kraken/streams/?game=Fortnite";
+        // Constructing URL
+        String game_chosen = User_Preferences.getSelection().replaceAll("\\s","");
+        String url = "https://api.twitch.tv/kraken/streams/?game=" + game_chosen;
 
-        // STRUCTURE OF JSON FOR Streams
-        //_total
-        //streams:
-//                {"_id":28397511456,
-//                "game":"Fortnite",
-//                "broadcast_platform":"live",
-//                "community_id":"558ffafb-6f75-44ff-8cb7-a4f045d2399f",
-//                "community_ids":["558ffafb-6f75-44ff-8cb7-a4f045d2399f", "8ea1e94f-4015-4f38-bd7e-745d8f42539a"],
-//                "viewers":15493,
-//                "video_height":1080,
-//                "average_fps":60,
-//                "delay":0,
-//                "created_at":"2018-04-20T02:42:40Z",
-//                "is_playlist":false,
-//                "stream_type":"live",
-//                "preview":{
-//                    "small":"https:\/\/static-cdn.jtvnw.net\/previews-ttv\/live_user_highdistortion-80x45.jpg",
-//                    "medium":"https:\/\/static-cdn.jtvnw.net\/previews-ttv\/live_user_highdistortion-320x180.jpg",
-//                    "large":"https:\/\/static-cdn.jtvnw.net\/previews-ttv\/live_user_highdistortion-640x360.jpg",
-//                    "template":"https:\/\/static-cdn.jtvnw.net\/previews-ttv\/live_user_highdistortion-{width}x{height}.jpg"},
-//                "channel":{
-//                    "mature":true,
-//                    "status":"TSM HighDistortion | 500 BITS FOR 99 cents with !Prime | #2 Kills on PC | ▶️ Youtube.com\/HighDistortion",
-//                    "broadcaster_language":"en",
-//                    "display_name":"HighDistortion",
-//                    "game":"Fortnite",
-//                    "language":"en",
-//                    "_id":84752541,
-//                    "name":"highdistortion",
-//                    "created_at":"2015-03-08T21:23:04.274013Z",
-//                    "updated_at":"2018-04-20T16:07:22.832027Z",
-//                    "partner":true,
-//                    "logo":"https:\/\/static-cdn.jtvnw.net\/jtv_user_pictures\/a86c03dc-a682-4069-b6b1-592847a8d078-profile_image-300x300.png",
-//                    "video_banner":"https:\/\/static-cdn.jtvnw.net\/jtv_user_pictures\/385231343fd2ef63-channel_offline_image-1920x1080.png",
-//                    "profile_banner":"https:\/\/static-cdn.jtvnw.net\/jtv_user_pictures\/ecc65383-4237-494b-bb0e-20a1f0f6eaa9-profile_banner-480.png",
-//                    "profile_banner_background_color":"","url":"https:\/\/www.twitch.tv\/highdistortion",
-//                    "views":4087967,
-//                    "followers":364575,
-//                    "broadcaster_type":"",
-//                    "description":"Thanks for stumbling on in this channel. You are watching Jimmy A.K.A. HighDistortion. Once a Pro gamer on the MLG circuit, now just an amateur bodybuilder who likes to lift and play a lot of video games.",
-//                    "private_video":false,
-//                    "privacy_options_enabled":false}
-//                }
+        // DEBUGGING
+        Log.d("STREAM URL", url);
 
-
-
-        //"Kraken" is deprecated
-        // a query for pokemon streams
-        // https://api.twitch.tv/kraken/search/streams?query=poker&client_id=CLIENTID
-        // https://api.twitch.tv/kraken/streams/featured
 
         JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
                 url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 Log.d("Prints", "length of response: " + response.length() + response.toString());
-
+                try {
                 // "data" is the name of the TwitchAPI JSON Array
                 // This array gets divided into objects (gameObject)
-                try {
+                    ArrayList<String> info = new ArrayList<>();
                     String gamesJSONString = response.getString("streams");
+
                     JSONArray jsonArr = new JSONArray(gamesJSONString);
-                    for (int i = 0; i < 1; i++) {
+                    if (indexExists(jsonArr, 0)){
+                        JSONObject jsonObj = jsonArr.getJSONObject(0);
+
+                        //Retrieve all values
+                        String game = recurseKeys(jsonObj, "game");
+                        String viewers = recurseKeys(jsonObj, "viewers");
+                        String name = recurseKeys(jsonObj, "name");
+                        String status = recurseKeys(jsonObj, "status");
+                        String updated_last = recurseKeys(jsonObj, "updated_at");
+                        String followers = recurseKeys(jsonObj, "followers");
+                        String description = recurseKeys(jsonObj, "description");
+                        String large_image = recurseKeys(jsonObj, "medium");
+
+                        info.add(game);
+                        info.add(name);
+                        info.add(viewers);
+                        info.add(followers);
+                        info.add(large_image);
+                        info.add("Last updated: " + updated_last);
+                        info.add(status);
+                        info.add(description);
+
+
                         // For every entry in the JSONArray make an object and get its name as well as box art.
-                        JSONObject gameObject = jsonArr.getJSONObject(0);
-                        Log.d("Prints", gameObject.toString());
+                        Log.d("Prints ID", recurseKeys(jsonObj, "name"));
+
+                    } else {
+                        info.add("No information available from Twitch.");
                     }
+                    setPopUpContent(info);
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -143,6 +147,40 @@ public class Pop extends Activity {
 
         };
         queue.add(jsonObjReq);
-        System.out.println(queue);
+
+    }
+
+    public static String recurseKeys(JSONObject jObj, String findKey) throws JSONException {
+        String finalValue = "";
+        boolean _stop = false;
+        if (jObj == null) {
+            return "";
+        }
+
+        Iterator<String> keyItr = jObj.keys();
+        Map<String, String> map = new HashMap<>();
+
+        while(keyItr.hasNext() || _stop == true) {
+            String key = keyItr.next();
+            map.put(key, jObj.getString(key));
+        }
+
+        for (Map.Entry<String, String> e : (map).entrySet()) {
+            String key = e.getKey();
+            if (key.equalsIgnoreCase(findKey)) {
+                _stop = true;
+                return jObj.getString(key);
+            }
+
+            // read value
+            Object value = jObj.get(key);
+
+            if (value instanceof JSONObject) {
+                finalValue = recurseKeys((JSONObject)value, findKey);
+            }
+        }
+
+        // key is not found
+        return finalValue;
     }
 }
